6970ba0e2fd79e0bd71cbe3f445066c6e356f92b
unknown <challasandeep05@gmail.com>
2025-03-26 13:15:58 +0530

    files added

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http")
+const PORT = 3000
+
+
+const server = http.createServer((req, res) => {
+    res.end("Hello")
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on:http://localhost${PORT}`);
+
+})
